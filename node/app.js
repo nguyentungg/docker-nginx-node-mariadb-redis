c362115458551d8caf1906c39e166ce31d12cff5
Nguyen Tung <thanhtung4693@gmail.com>
2023-07-29 18:03:42 +0700

    First commit

---
+++ node/app.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const app = express()
+const port = 3000
+const path = __dirname + '/view/';
+
+app.get('/', (req, res) => {
+  res.sendFile(path + 'index.html');
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
